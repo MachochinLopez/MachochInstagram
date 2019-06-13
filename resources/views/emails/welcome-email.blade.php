d039a38885092117d4df3f6a9c0d9dedf867b975
Oscar Alejandro Lopez Ramos <machochinnio@hotmail.com>
2019-06-13 04:09:21 -0500

    Initial commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to MachochInstagram!
+
+This is a community of fellow people and we love that you have joined us.
+
+All the best,<br>
+Machochin - MachochInstagram Director and Owner
+{{ config('app.name') }}
+@endcomponent
